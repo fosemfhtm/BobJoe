831555440b6227315401565dbe24920e2dfac36c
Dookong <dookong727@gmail.com>
2022-01-03 15:46:41 +0900

    navermap marker

--- app/src/main/java/food/map/MapViewFragment.kt
+++ app/src/main/java/food/map/MapViewFragment.kt
@@ -1,14 +1,17 @@
 package food.map
 
-import android.location.LocationManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.naver.maps.geometry.LatLng
-import com.naver.maps.map.*
+import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
 import com.naver.maps.map.overlay.Marker
+import food.map.phone.JsonController
 
 class MapViewFragment: Fragment(), OnMapReadyCallback {
     private lateinit var v: View
@@ -53,21 +56,31 @@ class MapViewFragment: Fragment(), OnMapReadyCallback {
 
     override fun onResume() {
         super.onResume()
-        mapFragment.getMapAsync{
-            naverMap = it
-            putMarkers(naverMap)
-        }    }
+        mapFragment.getMapAsync(this)
+    }
 
     private fun putMarkers(p0: NaverMap) {
-        val marker = Marker()
-        marker.position = LatLng(37.526344, 126.884523)
-        marker.map = p0
+
     }
 
     override fun onMapReady(nm: NaverMap) {
         naverMap = nm
         putMarkers(naverMap)
 
+        val jsonController = JsonController(requireContext())
+        val pickerLocationList = jsonController.readFromJson2()
+        pickerLocationList.forEach {
+            naverMap.putMarkers(LatLng(it.y, it.x))
+        }
+
         naverMap.moveCamera(CameraUpdate.scrollTo(LatLng(36.3986924, 127.4024869)))
+    }
+
+    private fun NaverMap.putMarkers(latlng: LatLng) {
+        val marker = Marker()
+        marker.width = 50
+        marker.height = 80
+        marker.position = latlng
+        marker.map = this
     }
 }
